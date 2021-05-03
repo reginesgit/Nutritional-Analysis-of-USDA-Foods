08256cbf250481e1ede603856e080715791be305
reginesgit <regine.strey@gmail.com>
2021-05-03 19:46:24 -0400

    Security configuration updated!

--- get_foods.py
+++ get_foods.py
@@ -1,9 +1,17 @@
 import requests
+from config import API_KEY
 # from flask import jsonify
 
 def get_data():
     
-    x = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=Bread')
+    query = "Bread"
+
+    url = "https://api.nal.usda.gov/fdc/v1/foods/search?"
+
+    query_url = f"{url}api_key={API_KEY}&query={query}"
+
+    x = requests.get(query_url)
+    # x = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=Bread')
     # x = requests.get('https://api.nal.usda.gov/fdc/v1/foods/list?api_key=DEMO_KEY')
     # x = requests.get('https://api.nal.usda.gov/fdc/v1/json-spec?api_key=DEMO_KEY')
 
